e0179c886f4fe630b07b8195690ff6be3e7693e1
msodeveloper <m.soares2603@gmail.com>
2014-10-02 10:58:37 -0300

    fix module bug, rename folders

--- javascripts/app.min.js
+++ javascripts/app.min.js
@@ -2,8 +2,8 @@
   'use strict';
 
   // Define Modules
-  angular.module('todo.form');
-  angular.module('todo.shared');
+  angular.module('todo.form', []);
+  angular.module('todo.shared', []);
 
   angular.module('todo', [
     'todo.form',
